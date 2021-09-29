7c034397fa1a8c651cdacbf011b1256f017e7bbc
haochibai <hb174@duke.edu>
2021-09-29 15:42:44 +0800

    Optimized version P2
    Signed-off-by: haochibai <hb174@duke.edu>

--- ECE_550/Project2/D_Latch.v
+++
@@ -1,9 +0,0 @@
-module D_Latch(E,D,Q,Q_bar);
-	input E,D;
-	output Q,Q_bar;
-	wire w1,w2,D_bar;
-	not not1(D_bar,D);
-	and and1(w1,E,D_bar);
-	and and2(w2,D,E);
-	SR_Latch SR1(w1,w2,Q,Q_bar);
-endmodule
--- ECE_550/Project2/SR_Latch.v
+++
@@ -1,6 +0,0 @@
-module SR_Latch(R,S,Q,Q_bar);
-	input R,S;
-	output Q,Q_bar;
-	nor nor1(Q,R,Q_bar);
-	nor nor2(Q_bar,Q,S);
-endmodule
